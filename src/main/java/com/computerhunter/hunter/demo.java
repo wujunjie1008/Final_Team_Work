789dad6f1622c273551617ce54ebdcf02596b258
lokking <55021867+lokking@users.noreply.github.com>
2019-11-12 22:50:04 +0800

    031702242

--- src/main/java/com/computerhunter/hunter/demo.java
+++ src/main/java/com/computerhunter/hunter/demo.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class demo {
-    @GetMapping("/just")
+    @GetMapping("/硬件知识")
     public String hhh(){
         return "硬件知识";
     }
--- src/main/java/com/computerhunter/hunter/demoone.java
+++ src/main/java/com/computerhunter/hunter/demoone.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class demoone {
-    @RequestMapping("/hello6")
-    public String hello6(String m){
-        String a="fbf\nafaf";
-        return a;
+            @RequestMapping("/hello6")
+            public String hello6(String m){
+                String a="fbf\nafaf";
+                return a;
     }
 }
